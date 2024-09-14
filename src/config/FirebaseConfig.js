d3d3741d2e80242debd20f10562b2f323149b87b
mausooq <mauchoomausooq051@gmail.com>
2024-09-14 07:49:46 +0530

    first commit

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -5,15 +5,15 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  databaseURL: "",
-
-  appId: "",
+  apiKey: "AIzaSyBvO6nEqjmugENtbmF4OK6v-XCndfS71Ec",
+  authDomain: "mausooq-425ec.firebaseapp.com",
+  projectId: "mausooq-425ec",
+  storageBucket: "mausooq-425ec.appspot.com",
+  messagingSenderId: "298020524431",
+  appId: "1:298020524431:web:587b268e42e9c2203c3bfa",
+  measurementId: "G-PMD9VNDF36"
 };
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
